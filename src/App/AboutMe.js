b0185c57ff0ff54315e7a3722ba2eddc08808252
Tim Chapin <chapin.timothys@gmail.com>
2023-05-13 21:28:15 +0900

    Initial commit

---
+++ src/App/AboutMe.js
@@ -0,0 +1,26 @@
+import '../stylesheets/index.scss';
+
+function AboutMe() {
+  return (
+    <div className="about-me-card">
+      <div className="profile-img">
+        <img src="/assets/profile.jpeg" alt="Timothy Chapin" />
+      </div>
+      <div className="profile">
+        <div className="profile-info">
+          <p>Hello There!</p>
+          <p>I'm <b>Tim Chapin</b></p>
+          <h4>Full Stack Developer</h4>
+        </div>
+        <div className="social-links">
+          <img src="/assets/linkedin.svg" alt="linkedin link"></img>
+          <img src="/assets/github.svg" alt="github link"></img>
+          <img src="/assets/facebook.svg" alt="facebook link"></img>
+          <img src="/assets/line.svg" alt="line link"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import '../stylesheets/index.scss';
+import AboutMe from './AboutMe';
+
+function App() {
+  return (
+    <div className="App">
+      <AboutMe />
+    </div>
+  );
+}
+
+
+export default App;
