63282f3ac8282bf9e6cb638705134fd7dc9ac006
Tim Chapin <chapin.timothys@gmail.com>
2024-02-22 16:55:21 +0900

    Hide tokyo maps until new version is ready

--- src/App/Projects.js
+++ src/App/Projects.js
@@ -1,59 +1,144 @@
-function Projects( {projectRefs} ) {
+function Projects ({ projectRefs }) {
   return (
-    <div className="projects-container">
-      <div className="project">
-        <div className="project-header">
-          <div className="project-img">
-            <a target="_blank" rel="noopener noreferrer" href="https://www.teachtokyo.com/" alt="teach tokyo website">
-              <img src="/assets/TeachTokyo.png" alt="thumbnail"></img>
+    <div className='projects-container'>
+      <div className='project'>
+        <div className='project-header'>
+          <div className='project-img'>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.teachtokyo.com/'
+              alt='teach tokyo website'
+            >
+              <img src='/assets/TeachTokyo.png' alt='thumbnail'></img>
             </a>
           </div>
-          <div className="project-title">
+          <div className='project-title'>
             <h2>Teach Tokyo</h2>
           </div>
         </div>
-        <div className="project-body">
-          <p>Teach Tokyo is a quiz based strategy game to be used in the classroom as a warmup activity before lessons. It uses an interactive SVG map of Japan which students can conquer for their teams.</p>
+        <div className='project-body'>
+          <p>
+            Teach Tokyo is a quiz based strategy game to be used in the
+            classroom as a warmup activity before lessons. It uses an
+            interactive SVG map of Japan which students can conquer for their
+            teams.
+          </p>
         </div>
-        <div className="tech-stack">
+        <div className='tech-stack'>
           <ul>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/rails/rails-plain.svg" alt="rails icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/postgresql/postgresql-plain.svg" alt="postgres icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/heroku/heroku-plain.svg" alt="heroku icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/javascript/javascript-plain.svg" alt="javascript icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/html5/html5-plain.svg" alt="html5 icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/css3/css3-plain.svg" alt="css3 icon"></img></li>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/rails/rails-plain.svg'
+                alt='rails icon'
+              ></img>
+            </li>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/postgresql/postgresql-plain.svg'
+                alt='postgres icon'
+              ></img>
+            </li>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/heroku/heroku-plain.svg'
+                alt='heroku icon'
+              ></img>
+            </li>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/javascript/javascript-plain.svg'
+                alt='javascript icon'
+              ></img>
+            </li>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/html5/html5-plain.svg'
+                alt='html5 icon'
+              ></img>
+            </li>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/css3/css3-plain.svg'
+                alt='css3 icon'
+              ></img>
+            </li>
           </ul>
         </div>
       </div>
-      <div className="project">
-        <div className="project-header">
-          <div className="project-img">
-            <a target="_blank" rel="noopener noreferrer" href="https://www.tokyomaps.net/" alt="teach tokyo website">
-              <img src="/assets/TokyoMaps.png" alt="thumbnail"></img>
-            </a>
+      {/* <div className='construction'>
+        <div id='teach-tokyo' className='project'>
+          <div className="maintenance">
+            <h1>Under Maintenance</h1>
           </div>
-          <div className="project-title">
-            <h2>Tokyo Maps</h2>
+          <div className='project-header'>
+            <div className='project-img'>
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://www.tokyomaps.net/'
+                alt='teach tokyo website'
+              >
+                <img src='/assets/TokyoMaps.png' alt='thumbnail'></img>
+              </a>
+            </div>
+            <div className='project-title'>
+              <h2>Tokyo Maps</h2>
+            </div>
+          </div>
+          <div className='project-body'>
+            <p>
+              Tokyo Maps is a MapBox based application made to assist expats
+              looking to relocate to Tokyo.{' '}
+            </p>
+          </div>
+          <div className='tech-stack'>
+            <ul>
+              <li>
+                <img
+                  src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/rails/rails-plain.svg'
+                  alt='rails icon'
+                ></img>
+              </li>
+              <li>
+                <img src='/assets/mapbox.svg' alt='mapbox icon'></img>
+              </li>
+              <li>
+                <img
+                  src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/postgresql/postgresql-plain.svg'
+                  alt='postgres icon'
+                ></img>
+              </li>
+              <li>
+                <img
+                  src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/heroku/heroku-plain.svg'
+                  alt='heroku icon'
+                ></img>
+              </li>
+              <li>
+                <img
+                  src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/javascript/javascript-plain.svg'
+                  alt='javascript icon'
+                ></img>
+              </li>
+              <li>
+                <img
+                  src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/html5/html5-plain.svg'
+                  alt='html5 icon'
+                ></img>
+              </li>
+              <li>
+                <img
+                  src='https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/css3/css3-plain.svg'
+                  alt='css3 icon'
+                ></img>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="project-body">
-          <p>Tokyo Maps is a MapBox based application made to assist expats looking to relocate to Tokyo. My team gathered rental, safety, international school, and pet friendliness data for each ward, normalized it, and displayed that information to the user through a filter system on an interactive map of Tokyo. </p>
-        </div>
-        <div className="tech-stack">
-          <ul>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/rails/rails-plain.svg" alt="rails icon"></img></li>
-            <li><img src="/assets/mapbox.svg" alt="mapbox icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/postgresql/postgresql-plain.svg" alt="postgres icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/heroku/heroku-plain.svg" alt="heroku icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/javascript/javascript-plain.svg" alt="javascript icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/html5/html5-plain.svg" alt="html5 icon"></img></li>
-            <li><img src="https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/css3/css3-plain.svg" alt="css3 icon"></img></li>
-          </ul>
-        </div>
-      </div>
+      </div> */}
     </div>
   )
 }
 
-export default Projects;
+export default Projects
